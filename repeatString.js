9435d2e976e397a893ecdfbde067cdba190d8f6a
meenu <->
2023-09-06 17:38:08 +0530

    Make some changes

--- repeatString.js
+++ repeatString.js
@@ -1,21 +1,33 @@
 
-function repeat (str,num){
-   var input = "";
-   if (num > 0){
-    for (var i  = 0; i < num; i++){
-        input = input + str;
+//function repeat (str,num){
+  // var input = "";
+   //if (num > 0){
+    //for (var i  = 0; i < num; i++){
+       // input = input + str;
 
-    }
-    console.log (input);
-   } 
-   else{
-    console.log("error");
-   } 
-}
+   // }
+    //console.log (input);
+   //} 
+  // else{
 
-repeat("hey",4)
-repeat("hay",0)
-repeat("hay",-1)
+    
+   // console.log("error");
+  // } 
+//}
+
+//repeat("hey",4)
+//repeat("hay",0)
+//repeat("hay",-1)
+
+var sum = 0;
+function add(num){
+  for (var i = 1; i <= num; i++) {
+    sum = sum + i;
+  }
+    console.log(sum);
+
+  } 
+add(5);
 
 
 
