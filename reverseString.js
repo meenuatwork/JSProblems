322ed858e6ce6ee74b71727c5b43557dbc3cf3f9
meenu <->
2023-09-16 14:11:55 +0530

    use reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,14 @@
 
-let string = 'meenu yadav';
-for (var i = string.length-1; i >= 0; i--){
-    console.log(string[i]);
-} 
-
-
+// let string = 'meenu yadav';
+// for (var i = string.length-1; i >= 0; i--){
+//     console.log(string[i]);
+// } 
+function reverseString(str){
+    var NewString = "";
+ for (var i = str.length-1; i >= 0; i--){
+    NewString += str[i];
+ }
+ console.log(NewString);
+}
+reverseString("meenu yadav");
   
